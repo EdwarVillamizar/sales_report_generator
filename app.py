f766b56a78ac453c2dde6f87f325a347a026487c
Edwar Alfredo Villamizar Rincon <villamizar.edwar@gmail.com>
2025-02-10 22:00:55 -0500

    feat: punto numero 1 y 2 completo.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 '''
-1.Descarga del Archivo de Datos:
-    Se te proporcionará un archivo Excel (datos_ventas.xlsx) que contiene información sobre ventas de una empresa.
-    
-    Obtener las columnas:
+1.1 Se te proporcionará un archivo Excel (datos_ventas.xlsx) que contiene información sobre ventas de una empresa.
+
+1.2 Obtener las columnas:
+
     ID_Venta: Identificador único de la venta
     Fecha: Fecha de la venta
     Producto: Nombre del producto
@@ -23,4 +23,39 @@ datos_de_ventas = pd.read_excel("datos_ventas.xlsx", usecols=columnas_para_vista
 
 datos_de_ventas['Total_Venta'] = datos_de_ventas['Total_Venta'].fillna(datos_de_ventas['Cantidad']*datos_de_ventas['Precio_Unitario'])
 
+'''2.3 Convertir la columna Fecha a formato datetime.'''
+
+datos_de_ventas['Fecha'] = pd.to_datetime(datos_de_ventas['Fecha'])
+
+'''2.4 Filtrar las ventas del año 2023.'''
+
+datos_de_ventas = datos_de_ventas[datos_de_ventas['Fecha'].dt.year == 2023]
+
+'''2.5 Agregar una nueva columna Mes con el mes en formato numérico extraído de Fecha.'''
+
+datos_de_ventas['Mes']=datos_de_ventas['Fecha'].dt.month
+
+'''2.6 Calcular el total de ventas por vendedor y por mes.'''
+
+total_ventas_vendedor = datos_de_ventas.groupby('Vendedor')['Total_Venta'].sum().reset_index()
+
+total_ventas_mes = datos_de_ventas.groupby('Mes')['Total_Venta'].sum().reset_index()
+
+
+
+
+
+
+
 print(datos_de_ventas)
+
+
+'''
+Calcular el total de ventas por vendedor y por mes.
+Generación de un Nuevo Archivo Excel:
+Crear un nuevo archivo Excel (resumen_ventas.xlsx) con dos hojas:
+"Resumen_Ventas": Contiene el total de ventas por vendedor.
+"Ventas_Mensuales": Contiene el total de ventas por mes.
+Entrega:
+Compartir el archivo resumen_ventas.xlsx generado.
+Compartir el link a un repositorio de GitHub usando el correctamente el manejo de commits.'''
